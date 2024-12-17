1d63889aa0c5d5e2a1e714e03da884955c03bb06
yoonguri988 <56820897+yoonguri988@users.noreply.github.com>
2024-12-17 15:47:01 +0900

    Fix: Routes로 페이지 나누기

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,23 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./components/App";
 import HomePage from "./pages/HomePage";
+import CourseListPage from "./pages/CourseListPage";
+import CoursePage from "./pages/CoursePage";
+import WishlistPage from "./pages/WishlistPage";
 
 function Main() {
   return (
     <BrowserRouter>
       <App>
-        <HomePage />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="course" element={<CourseListPage />} />
+          <Route
+            path="course/react-frontend-development"
+            element={<CoursePage />}
+          />
+          <Route path="wishlist" element={<WishlistPage />} />
+        </Routes>
       </App>
     </BrowserRouter>
   );
