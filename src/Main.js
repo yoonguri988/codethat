06f9d1ad3052071e826860b48a66c01207a3771a
yoonguri988 <56820897+yoonguri988@users.noreply.github.com>
2024-12-18 16:22:13 +0900

    Fix: (실습) 커뮤니티 글 보여주기

--- src/Main.js
+++ src/Main.js
@@ -19,7 +19,7 @@ function Main() {
           </Route>
           <Route path="questions">
             <Route index element={<QuestionListPage />} />
-            <Route path="616825" element={<QuestionPage />} />
+            <Route path=":questionId" element={<QuestionPage />} />
           </Route>
           <Route path="wishlist" element={<WishlistPage />} />
         </Route>
