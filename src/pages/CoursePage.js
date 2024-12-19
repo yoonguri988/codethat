b17bffd7d34a13083976df3250c403efec8727c0
yoonguri988 <56820897+yoonguri988@users.noreply.github.com>
2024-12-19 15:09:06 +0900

    Fix: Navigate로 리다이렉트 하기

--- src/pages/CoursePage.js
+++ src/pages/CoursePage.js
@@ -5,12 +5,16 @@ import Card from "../components/Card";
 import CourseIcon from "../components/CourseIcon";
 import getCourseColor from "../utils/getCourseColor";
 import styles from "./CoursePage.module.css";
-import { useParams } from "react-router-dom";
+import { Navigate, useParams } from "react-router-dom";
 
 function CoursePage() {
   const { courseSlug } = useParams();
   const course = getCourseBySlug(courseSlug);
   const courseColor = getCourseColor(course?.code);
+
+  if (!course) {
+    return <Navigate to="/courses" />;
+  }
 
   const headerStyle = {
     borderTopColor: courseColor,
